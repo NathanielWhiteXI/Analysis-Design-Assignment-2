dac732eb93e8cc6b61d801da5fa65e663585a2b5
Ikbal K. <144921923+iqbalkhosti@users.noreply.github.com>
2025-11-19 12:36:16 -0500

    question1-solution

---
+++ question1.py
@@ -0,0 +1,34 @@
+
+def min_distance(string1, string2):
+	
+	m  = len(string1) #columns
+	n  = len(string2) # rows
+
+	table = [[0]*(n+1) for i in range(m+1)]
+
+	for i in range (m+1):
+		table [i][0]=i
+
+	for j in range (n+1):
+		table[0][j] = j
+
+	
+
+	for i in range(1, m+1):
+		for j in range(1, n+1):
+			if (string1[i-1] == string2[j-1]):
+
+				table[i][j]=table[i-1][j-1]
+			else:
+				table[i][j] = 1+ min( table[i-1][j], table[i][j-1], table[i-1][j-1])
+
+	return table[m][n]
+	
+
+print(min_distance("kitten", "sitting"))
+print(min_distance("", "abc"))
+print(min_distance("hello", ""))
+print(min_distance("intention", "execution"))
+
+	
+	
